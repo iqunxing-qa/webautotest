7005afee0493cd5f7656c1e6e2991ac6299f2887
rongjing.jiang <rongjing.jiang@dcfservice.net>
2016-04-22 14:02:17 +0800

    update

--- Pythonscripts/classmethod/login.py
+++ Pythonscripts/classmethod/login.py
@@ -8,17 +8,18 @@ host=cf.get('service','host')
 method=cf.get('dir','method')
 data=cf.get('dir','data')
 
-def corp_login(self,csv):
+def corp_login(self,csvfile):
      browser=self.browser
-     csvpaths=file(''+data+'csv', 'rb')
+     csvpaths=file(''+data+csvfile, 'rb')
      f = csv.reader(csvpaths)
      browser.get('http://'+host+'.dcfservice.com/login.jsp')
      for line in f:
          #list=line.replace("\n","").split(",")
          #print list
-         corname=line[0]
-         username=line[1]
-         password=line[2]
+         corname=line[0].decode('utf-8')
+         username=line[1].decode('utf-8')
+         password=line[2].decode('utf-8')
+
          browser.find_element_by_id('corp_name').clear()
          browser.find_element_by_id('corp_name').send_keys(corname)
          browser.find_element_by_id('j_user_name').clear()
@@ -27,16 +28,16 @@ def corp_login(self,csv):
          browser.find_element_by_id('j_password').send_keys(password)
 
 
-def operate_login(self,csv):
+def operate_login(self,csv1):
      browser=self.browser
-     csvpaths=file(''+data+'csv', 'rb')
+     csvpaths=file(''+data+csv1, 'rb')
      f = csv.reader(csvpaths)
      browser.get('http://'+host+'.dcfservice.com/loginop.jsp')
      for line in f:
          #list=line.replace("\n","").split(",")
          #print list
-         username=line[0]
-         password=line[1]
+         username=line[0].decode('utf-8')
+         password=line[1].decode('utf-8')
          browser.find_element_by_id('j_user_name').clear()
          browser.find_element_by_id('j_user_name').send_keys(username)
          browser.find_element_by_id('j_password').clear()
