86bafa34596ec36580c40acd0e071e4eaad055e7
rongjing.jiang <rongjing.jiang@dcfservice.net>
2016-04-28 15:44:06 +0800

    add

---
+++ Pythonscripts/webtestcases/test_flow4.py
@@ -0,0 +1,146 @@
+__author__ = 'MP-4DRH8'
+#coding=utf-8
+from selenium import webdriver
+import time
+import unittest
+import ConfigParser
+import os
+import mysql.connector
+from classmethod import login
+cf = ConfigParser.ConfigParser()
+cf.read(r"D:\Workspace\Pythonscripts\environment\env.conf")
+host=cf.get('service','host')
+method=cf.get('dir','method')
+data=cf.get('dir','data')
+#读取数据库文件
+USER=cf.get('dcf_contract','user')
+HOST=cf.get('dcf_contract','host')
+PASSWORD=cf.get('dcf_contract','password')
+PORT=cf.get('dcf_contract','port')
+DATABASE=cf.get('dcf_contract','database')
+class Core_Enterprise(unittest.TestCase):
+    (u"核心模块")
+    @classmethod
+    def setUpClass(cls):
+        cls.browser = webdriver.Firefox()
+        cls.browser.maximize_window()
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.close()
+        cls.browser.quit()
+
+    def test_1(self):
+        (u"新建机构工作方式")
+        browser = self.browser
+        agencyName='test9'
+
+        login.operate_login(self,'operation_login.csv') #登陆
+        time.sleep(2)
+        browser.find_element_by_link_text(u"产品配置").click()
+        time.sleep(2)
+        browser.find_element_by_link_text(u"机构工作方式").click()
+        time.sleep(2)
+        browser.find_element_by_id('new-orgMethod').click()
+        browser.find_element_by_id('agencyName').send_keys(agencyName)#融资申请模板
+        we=browser.find_element_by_xpath("//div[@id='fileuploadApplyAreaDiv']/div/div/span")
+        browser.execute_script("arguments[0].scrollIntoView()",we)
+        time.sleep(2)
+        we.click()
+        time.sleep(2)
+        os.system("D:\workspace\Pythonscripts\classmethod\upload_excel.exe")
+        browser.find_element_by_id('fileuploadModelName').send_keys('protocol_document')#协议文档
+        time.sleep(2)
+        browser.find_element_by_xpath("//input[@id='fileuploadModel']/ancestor::div[1]/span").click()
+        time.sleep(2)
+        os.system("D:\workspace\Pythonscripts\classmethod\upload_word.exe")
+        time.sleep(2)
+        browser.find_element_by_xpath("//span[text()='+创建模板']").click()
+        browser.find_element_by_id('fileuploadControlName').send_keys('control_document')#操作说明文档
+        time.sleep(2)
+        browser.find_element_by_xpath("//input[@id='fileuploadControl']/ancestor::div[1]/span").click()
+        os.system("D:\workspace\Pythonscripts\classmethod\upload_PDF.exe")
+        time.sleep(2)
+        browser.find_element_by_xpath("//span[text()='+创建操作说明']").click()
+        browser.find_element_by_id('fileuploadContractName').send_keys('contract_document')#微合同文档
+        time.sleep(2)
+        browser.find_element_by_xpath("//input[@id='fileuploadContract']/ancestor::div[1]/span").click()
+        os.system("D:\workspace\Pythonscripts\classmethod\upload_TXT.exe")
+        time.sleep(2)
+        browser.find_element_by_xpath("//span[text()='+创建微合同']").click()
+        browser.find_element_by_id('creatAgency').click()
+        time.sleep(2)
+        browser.find_element_by_link_text(u'返回列表').click()
+        time.sleep(2)
+        browser.find_element_by_id('search-button').click()
+        time.sleep(2)
+        #检验是否新建成功
+        try:
+           # 数据库连接
+           conn = mysql.connector.connect(host=HOST,user=USER,passwd=PASSWORD,db=DATABASE,port=PORT)
+           # 创建游标
+           cur = conn.cursor()
+           # institution_process_model_pkey查询
+           sql='select institution_process_model_pkey from t_institution_process_model where institution_process_model_name="' + agencyName + '"'
+           cur.execute(sql)
+           # 获取查询结果
+           result_set = cur.fetchall()
+           if result_set:
+              for row in result_set:
+                 institution_id = row[0]
+                 print institution_id
+           else:
+              print "No date"
+           # 关闭游标和连接
+           cur.close()
+           conn.close()
+        except mysql.connector.Error, e:
+            print e.message
+        institution_id = str(institution_id)
+        path="//tr[@id=" + institution_id + "]/td[text()="+agencyName+"]"
+        if browser.find_element_by_xpath(path).is_displayed():
+            print 'New Success ！'
+        else:
+            print 'New Fail ！'
+
+    def test_2(self):
+        (u"启用机构工作方式")
+        browser = self.browser
+        agencyName='test9'
+
+        login.operate_login(self,'operation_login.csv') #登陆
+        time.sleep(2)
+        browser.find_element_by_link_text(u"产品配置").click()
+        time.sleep(2)
+        browser.find_element_by_link_text(u"机构工作方式").click()
+        time.sleep(2)
+        try:
+           # 数据库连接
+           conn = mysql.connector.connect(host=HOST,user=USER,passwd=PASSWORD,db=DATABASE,port=PORT)
+           # 创建游标
+           cur = conn.cursor()
+           # institution_process_model_pkey查询
+           sql='select institution_process_model_pkey from t_institution_process_model where institution_process_model_name="' + agencyName + '"'
+           cur.execute(sql)
+           # 获取查询结果
+           result_set = cur.fetchall()
+           if result_set:
+              for row in result_set:
+                 institution_id = row[0]
+                 print institution_id
+           else:
+              print "No date"
+           # 关闭游标和连接
+           cur.close()
+           conn.close()
+        except mysql.connector.Error, e:
+            print e.message
+        institution_id = str(institution_id)
+        path="//tr[@id=" + institution_id + "]/following::td[4]/a[3]"
+        browser.find_element_by_xpath(path).click()
+        time.sleep(3)
+        browser.find_element_by_id('modalBtn').click()
+        time.sleep(1)
+        if browser.find_element_by_xpath("").is_displayed():
+            print 'Start Success！'
+        else:
+            print 'Start Fail ！'
---
+++ Pythonscripts/webtestcases/test_flow5.py
@@ -0,0 +1,38 @@
+__author__ = 'MP-4DRH8'
+#coding=utf-8
+from selenium import webdriver
+import time
+import unittest
+import ConfigParser
+from classmethod.getdate import get_date
+import os
+from classmethod import login
+cf = ConfigParser.ConfigParser()
+cf.read(r"D:\Workspace\Pythonscripts\environment\env.conf")
+host=cf.get('service','host')
+method=cf.get('dir','method')
+data=cf.get('dir','data')
+
+class Core_Enterprise(unittest.TestCase):
+    (u"核心模块")
+    @classmethod
+    def setUpClass(cls):
+        cls.browser = webdriver.Firefox()
+        cls.browser.maximize_window()
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.close()
+        cls.browser.quit()
+    def test(self):
+        # (u"新建方案")
+        # browser = self.browser
+        # login.operate_login(self,'operation_login.csv') #登陆
+        # time.sleep(2)
+        # browser.find_element_by_link_text(u"产品配置").click()
+        # time.sleep(2)
+        # browser.find_element_by_link_text(u"方案配置").click()
+        # time.sleep(2)
+        # browser.find_element_by_id('new-program').click()
+        # browser.find_element_by_id('product').click()
+        num=get_date.date
+        print( num)
